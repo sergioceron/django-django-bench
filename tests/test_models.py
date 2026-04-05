cf7059ee4b52e282f0b6c53865d9f1e6a9fd9b7b
CoderOps <coderops@coderops.dev>
2026-04-05 05:22:22 +0000

    fix: automated changes by CoderOps

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,19 +1,18 @@
 from django.test import TestCase
 from myapp.models import MyModel
-from django.db import models
 # Existing test case classes
 
-class MyModelTestCase(TestCase):
+class MyModelPKClearingTestCase(TestCase):
     def setUp(self):
         # Create a model instance
-        self.instance = MyModel.objects.create(field1='value1', field2='value2')
+        self.instance = MyModel.objects.create()
 
-    def test_pk_cleared_post_deletion(self):
+    def test_pk_set_to_none_after_deletion(self):
         # Ensure the instance has a PK before deletion
         self.assertIsNotNone(self.instance.pk)
 
         # Delete the instance
         self.instance.delete()
 
-        # Ensure the PK is None after deletion
+        # Ensure the PK of the instance is set to None after deletion
         self.assertIsNone(self.instance.pk)
