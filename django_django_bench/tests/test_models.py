44bc6c8081b12540ecf401711360faf268668189
CoderOps <coderops@coderops.dev>
2026-04-05 05:38:29 +0000

    fix: automated changes by CoderOps

--- django_django_bench/tests/test_models.py
+++ django_django_bench/tests/test_models.py
@@ -1,5 +1,5 @@
 def test_delete_model_instance(self):
         instance = MyModel.objects.create(name='Test')
         instance.delete()
-        self.assertFalse(MyModel.objects.filter(id=instance.id).exists())
-        self.assertIsNone(instance.id)
+        self.assertFalse(MyModel.objects.filter(id=instance.pk).exists())
+        self.assertIsNone(instance.pk)
