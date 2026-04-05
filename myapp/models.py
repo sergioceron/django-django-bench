9f763d3c100fb15479a1f89a55eeefc27f446136
CoderOps <coderops@coderops.dev>
2026-04-05 05:25:28 +0000

    fix: automated changes by CoderOps

--- myapp/models.py
+++ myapp/models.py
@@ -1,4 +1,6 @@
 def delete(self, using=None, keep_parents=False):
+        super().delete(using=using, keep_parents=keep_parents)
+        self.pk = None
         pk = self.pk
         super().delete(using=using, keep_parents=keep_parents)
         if pk:
